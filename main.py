7a7e9ca1e5f76dc560e8452c20a0fe547d0d5873
Najmul3581 <126800158+Najmul3581@users.noreply.github.com>
2024-10-18 23:39:24 +0600

    Mad libs generator

---
+++ main.py
@@ -0,0 +1,20 @@
+# username = input("What Is Your Name:")
+# fathername = input("What Is Your Father Name:")
+# mothername = input("What Is Your Mother Name:")
+# study = input("Which Class Do You Read In:")
+# relationship = input("Tell Me About Your Relationship Status:")
+#
+# print(f"My Doggy Name Is {username}")
+
+q1 = input("Country?")
+q2 = input("Verb ending in -ing?")
+q3 = input("Noun?")
+q4 = input("Adjective?")
+q5 = input("Body part?")
+q6 = input("Plural noun?")
+q7 =input("Exclamation?")
+
+print(f"Last summer, I went to{q1},for vacation. I was so excited to spend my days{q2},"
+      f"But things took a turn when I accidentally packed a{q3},in my suitcase! After getting{q4}"
+      f"sunburn on my{q5},I ran into a group of{q6},I screamed,{q7},and decided it was time to go home."
+      )
